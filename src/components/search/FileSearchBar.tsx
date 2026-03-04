16c3f3176e7da988ed001db24fe9bdf95f4ec419
amavankal <amavankal@gmail.com>
2026-03-04 17:02:28 -0600

    Add basic input handlings structure to Autocomplete component

--- src/components/search/FileSearchBar.tsx
+++ src/components/search/FileSearchBar.tsx
@@ -2,15 +2,18 @@
 
 import {
   Autocomplete,
+  TextField,
 } from '@mui/material';
 
 import useDebounce from '@utils/useDebounce';
 import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { useTRPC } from '@src/trpc/react';
 
 export const FileSearchBar = () => {
   const [input, setInput] = useState("");
+  const router = useRouter();
   const trpc = useTRPC();
 
   const debouncedInput = useDebounce(input, 300);
@@ -27,6 +30,26 @@ export const FileSearchBar = () => {
 
 	return (
 		<Autocomplete
+			freeSolo
+			inputValue={input}
+			options={input === '' ? [] : (data ?? [])}
+			filterOptions={(o) => o}
+			onChange={(event, value, reason) => {
+				if (reason == 'selectOption') {
+					
+				} else if (reason == 'createOption') { // no match
+
+				}
+			}}
+			onInputChange={(e, value) => {
+				setInput(value);
+			}}
+			getOptionLabel={(option) => {
+				return typeof option === 'string' ? option : option.name;
+			}}
+			getOptionKey={(option) => {
+				return typeof option === 'string' ? option : option.id;
+			}}
 		/>
   );
 }
