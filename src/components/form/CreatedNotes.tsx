647dc8bedad952bf317001c9cc7ec93b56653616
nishthefish06 <nishanth06.us@gmail.com>
2026-04-08 18:09:05 -0500

    fixes

--- src/components/form/CreatedNotes.tsx
+++ src/components/form/CreatedNotes.tsx
@@ -25,9 +25,9 @@ export default function CreatedNotes({
         gridClassName={gridClassName}
         noFilesMessage={
           noFilesMessage ?? (
-          <div className="w-full py-12 flex flex-col items-center justify-center border-2 border-dashed border-slate-200 dark:border-slate-800 rounded-xl text-md font-medium text-slate-600 dark:text-slate-400">
-            You haven&apos;t created any notes yet.
-          </div>
+            <div className="w-full py-12 flex flex-col items-center justify-center border-2 border-dashed border-slate-200 dark:border-slate-800 rounded-xl text-md font-medium text-slate-600 dark:text-slate-400">
+              You haven&apos;t created any notes yet.
+            </div>
           )
         }
       />
