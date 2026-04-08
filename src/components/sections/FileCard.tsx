5520e66f3b8e92eb56a712eb6edb7f966ead3fe4
SXT230118 <sxt230118@utdallas.edu>
2026-04-08 00:33:05 -0500

    Clicking a note now navigates to /notes/[id] instead of opening the PDF in a separate tab

--- src/components/sections/FileCard.tsx
+++ src/components/sections/FileCard.tsx
@@ -80,9 +80,7 @@ export default function FileCard({ file }: FileCardProps) {
   return (
     <BaseCard variant="interactive" className="flex h-full flex-col">
       <Link
-        href={file.publicUrl}
-        target="_blank"
-        rel="noreferrer"
+        href={`/notes/${file.id}`}
         className="flex grow flex-col"
       >
         <div className="overflow-hidden rounded-t-lg border-b border-neutral-200 bg-white dark:border-neutral-600 dark:bg-neutral-700">
