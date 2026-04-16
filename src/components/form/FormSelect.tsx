12228a59c0400c71cd95bf8ea7db4cab439cd6f9
saikarthikbattula <80858606+saikarthikbattula@users.noreply.github.com>
2026-04-16 12:54:58 -0500

    Updates

--- src/components/form/FormSelect.tsx
+++ src/components/form/FormSelect.tsx
@@ -49,12 +49,12 @@ export default function FormSelect({
     <FormControl className={`w-64 ${className}`}>
       <InputLabel>{label}</InputLabel>
       <GenericSelect
-        value={field.state.value ?? ''}
+        value={field.state.value}
         onBlur={field.handleBlur}
         onChange={(e) => field.handleChange(e.target.value)}
         className="bg-white dark:bg-neutral-800"
         size="small"
-        error={!!field.state.value && !field.state.meta.isValid}
+        error={!field.state.meta.isValid}
         label={label}
         displayEmpty
         {...props}
@@ -71,11 +71,11 @@ export default function FormSelect({
           </MenuItem>
         ))}
       </GenericSelect>
-      {!!field.state.value && !field.state.meta.isValid && (
-        <FormHelperText>
-          {field.state.meta.errors.map((err) => err?.message).join('. ') + '.'}
-        </FormHelperText>
-      )}
+      <FormHelperText>
+        {!field.state.meta.isValid
+          ? field.state.meta.errors.map((err) => err?.message).join('. ') + '.'
+          : undefined}
+      </FormHelperText>
     </FormControl>
   );
 }
