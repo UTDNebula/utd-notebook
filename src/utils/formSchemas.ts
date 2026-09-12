f3f5e2abc57cc4efc8d9d232a8830c0b7782a5ef
Natalie 3 <natalie.nenx@gmail.com>
2026-09-12 04:02:32 -0500

    Require last name, major, minor, and date(*) in settings and onboarding schemas
    (*) There was an error message for needing graduation date in the code, but the logic allows it to be nullable, aka not required. Not sure if we want graduation date to be nullable, so I haven't done anything about it.

--- src/utils/formSchemas.ts
+++ src/utils/formSchemas.ts
@@ -23,7 +23,8 @@ export const accountSettingsSchema = z.object({
   major: z.string().min(1, 'College major is required'),
   minor: z.string().nullable(),
   studentClassification: z.enum(studentClassificationEnum.enumValues),
-  graduationDate: z.date().nullable(),
+  // date is not actually required by the logic right now. it can be null.
+  graduationDate: z.date('Graduation date is required').nullable(),
   contactEmail: z
     .email({
       error: 'Use your UT Dallas email',
@@ -36,12 +37,12 @@ export const accountSettingsSchema = z.object({
 export type AccountSettingsSchema = z.infer<typeof accountSettingsSchema>;
 
 export const accountOnboardingSchema = z.object({
-  firstName: z.string().min(1, 'Name is required'),
-  lastName: z.string().optional(),
-  major: z.string().optional(),
+  firstName: z.string().min(1, 'First name is required'),
+  lastName: z.string().min(1, 'Last name is required'),
+  major: z.string().min(1, 'College major is required'),
   minor: z.string().nullable().optional(),
   studentClassification: z.enum(studentClassificationEnum.enumValues),
-  graduationDate: z.date({ error: 'Graduation date is required' }).nullable(),
+  graduationDate: z.date('Graduation date is required').nullable(),
   contactEmail: z
     .email({
       error: 'Use your UT Dallas email',
